a103c6e8271cf5f2feb95e18981125fc12f17854
pcoacci <pcoacci@originsoftware.com.ar>
2022-01-06 16:27:21 -0300

    prueba flatlist

--- src/screens/GameManagerScreen/index.js
+++ src/screens/GameManagerScreen/index.js
@@ -1,9 +1,72 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, FlatList, Button } from 'react-native';
 import style from './styles';
 import * as GameRepository from '../../repositories/GamesRepository';
 
 export default function GameManagerScreen({ navigation, route }) {
+
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
 
   const [gameName, setGameName] = useState('');
 
@@ -11,6 +74,28 @@ export default function GameManagerScreen({ navigation, route }) {
     console.log(gameName);
     alert('hola mundo');
   }
+
+  const prueba = (textito) => {
+    console.log('entro');
+    console.log(textito);
+    alert('hola mundo2');
+  }
+
+  const Item = ({ title }) => (
+    <View style={style.item}>
+      <Text style={style.title}>{title}</Text>
+      <TouchableOpacity
+          
+          onPress={() => {prueba(title)}}
+        >
+          <Text>texto boton</Text>
+        </TouchableOpacity>
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
 
   return (
       <View style={style.container}>
@@ -28,6 +113,11 @@ export default function GameManagerScreen({ navigation, route }) {
         >
           <Text>Crear Partida</Text>
         </TouchableOpacity>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
       </View>
     );
   }
--- src/screens/GameManagerScreen/styles.js
+++ src/screens/GameManagerScreen/styles.js
@@ -21,7 +21,16 @@ const style = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
-  }
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
 
 export default style;
